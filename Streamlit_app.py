cff4eb284203c8d64dc82c423b1fe583ae2cb839
Gimmixboi <127659666+Gimmixboi@users.noreply.github.com>
2023-03-14 23:41:20 +0700

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
