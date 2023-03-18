f418e524ee67fe2078ec969971ba72735a8f7d91
Gimmixboi <127659666+Gimmixboi@users.noreply.github.com>
2023-03-18 13:54:35 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt 
 
-# RUN pip --no-cache-dir install -r requirements.txt
-# import matplotlib.pyplot as plt 
-
-# from sklearn.cluster import KMeans
-# from sklearn.preprocessing import LabelEncoder
-# from sklearn.metrics import silhouette_score
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import silhouette_score
 
 st.write('Hello World')
