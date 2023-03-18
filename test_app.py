954b47b2e4ca6699246eca1e0cc8aaded325e9e7
Gimmixboi <127659666+Gimmixboi@users.noreply.github.com>
2023-03-18 13:28:30 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import silhouette_score
 
 st.write('Hello World')
