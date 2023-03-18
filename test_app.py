1cdc7980fe35c6cafed15d95b19fe95ec7b4ebd5
Gimmixboi <127659666+Gimmixboi@users.noreply.github.com>
2023-03-18 14:10:16 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -6,14 +6,10 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import silhouette_score
 
-
-#     st.set_page_config(page_title="Clustering Model with K-Means", page_icon=":clipboard:", layout="wide")
-  
-
-
-    st.sidebar.title("Clustering Model with K-Means")
-    # อัพโหลดไฟล์ csv
-    uploaded_file = st.file_uploader("Upload CSV or Excel file", type=["csv", "xlsx"])
+st.set_page_config(page_title="Clustering Model with K-Means", page_icon=":clipboard:", layout="wide")
+st.sidebar.title("Clustering Model with K-Means")
+# อัพโหลดไฟล์ csv
+uploaded_file = st.file_uploader("Upload CSV or Excel file", type=["csv", "xlsx"])
 
     # อ่านไฟล์ csv และแสดงตัวอย่างข้อมูล
     if uploaded_file is not None:
