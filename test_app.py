ad28bb7fe298bb61e092965feff9e145c408dd00
Gimmixboi <127659666+Gimmixboi@users.noreply.github.com>
2023-03-18 18:09:29 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -32,7 +32,8 @@ else:
         df['Channel'] = le.fit_transform(df['Channel'])
         df['SKU'] = le.fit_transform(df['SKU'])
         df['Product_type'] = le.fit_transform(df['Product_type'])
-        df['Order Quantity (Item)','Total Value'].astype('int')
+        df['Order Quantity (Item)'].astype('int64')
+        df['Total Value'].astype('int64')
          
         st.write(df.head())
         st.write("Data have "+df.shape[0]+" rows")
