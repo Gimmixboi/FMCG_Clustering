fbe2cbcaf60cd8fc5886fc6da3bae4252b75677d
Gimmixboi <127659666+Gimmixboi@users.noreply.github.com>
2023-03-18 13:28:50 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt 
+# import matplotlib.pyplot as plt 
 
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import LabelEncoder
