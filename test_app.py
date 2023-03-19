5ce61c58e3dffe002b31b02b3ee43667e76d8ffe
Gimmixboi <127659666+Gimmixboi@users.noreply.github.com>
2023-03-19 09:16:43 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -13,6 +13,7 @@ from sklearn.preprocessing import StandardScaler
 st.title("Clustering Model with K-Means on Web-Application 💻")
 
 def clean_data(df):
+    with st.spinner("Data is processing,  ⏰ Please wait..."):
     df.dropna(inplace=True)
     df.drop_duplicates(inplace=True)
     df.drop(['CustomerID','Document Date'], axis=1,inplace=True)
@@ -29,26 +30,21 @@ def clean_data(df):
     return df
 
 def run_clustering(df, n_clusters):
-    cleaned_df = clean_data(df)
-    with st.spinner("Program is Calculating,  Please wait..."):
+    with st.spinner("Program is Calculating,  ⏰ Please wait..."):
         wcss = []
-        silhouette_scores = []
         for i in range(2, 10):
             model = KMeans(n_clusters=i, init='k-means++', random_state=0)
             model.fit(cleaned_df.values)
             wcss.append(model.inertia_)
-            score = silhouette_score(cleaned_df, model.labels_)
-            silhouette_scores.append(score)
         fig, ax = plt.subplots()
-        st.write("📊 WCSS & Silhouette Score Graph for find proper K and re-modeling")
+        st.write("📊 WCSS Graph for find proper K and re-modeling")
         plt.figure(figsize=(8, 6))
-        ax.plot(range(1, 10), wcss)
+        ax.plot(range(2, 10), wcss)
         ax.set_title('The Elbow Method')
         ax.set_xlabel('Number of clusters')
         ax.set_ylabel('WCSS')
         st.pyplot(fig)
-        return model,silhouette_scores
-
+        return model
 
 def main():
     # อัพโหลดไฟล์ csv
@@ -63,13 +59,17 @@ def main():
         st.write("Uploaded file:")
         st.write(df.head())
         st.write(f"Data have {df.shape[0]} rows")
+        st.warning("Please cleansing data.")
+        # Clean data
+        if st.button('Cleansing data'):
+            cleaned_df = clean_data(df)
         # clustering
         if st.button('Run Clustering'):
-            n_clusters = st.slider('Number of Clusters', 1, 10, 2)
-            model, silhouette_scores = run_clustering(df, n_clusters)
+            n_clusters = st.slider('Number of Clusters', 2, 10, 2)
+            model = run_clustering(df, n_clusters)
+            score = silhouette_score(cleaned_df, model.labels_)
+            silhouette_scores.append(score)
             st.write(f'Silhouette Score: {silhouette_scores:.2f}')
             
 if __name__ == '__main__':
     main()
-
-   
