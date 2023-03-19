d50e71274d673413f15f11e1dac614fcf9aceab3
Gimmixboi <127659666+Gimmixboi@users.noreply.github.com>
2023-03-19 18:56:57 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -113,6 +113,30 @@ def main():
 #                 if button('Refine your clusters!',key='Remodeling'):
             remodeling(cleaned_df, n_clusters)
         else:    
+            if "button1" not in st.session_state:
+                st.session_state["button1"] = False
+
+            if "button2" not in st.session_state:
+                st.session_state["button2"] = False
+
+            if "button3" not in st.session_state:
+                st.session_state["button3"] = False
+
+            if st.button("Button1"):
+                st.session_state["button1"] = not st.session_state["button1"]
+
+            if st.session_state["button1"]:
+                if st.button("Button2"):
+                    st.session_state["button2"] = not st.session_state["button2"]
+
+            if st.session_state["button1"] and st.session_state["button2"]:
+                if st.button("Button3"):
+                    # toggle button3 session state
+                    st.session_state["button3"] = not st.session_state["button3"]
+
+            if st.session_state["button3"]:
+                st.write("**Button3!!!**")
+            
             st.warning("Please cleansing data first.")
       
 if __name__ == '__main__':
