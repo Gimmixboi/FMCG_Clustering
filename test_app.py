50121eae9608e2a5d20c8872961e3067355785c7
Gimmixboi <127659666+Gimmixboi@users.noreply.github.com>
2023-03-19 10:10:58 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -63,14 +63,16 @@ def main():
         # Clean data
         if st.button('Cleansing data'):
             cleaned_df = clean_data(df)
+            is_cleaned = True
         # clustering
-            if st.button('Run Clustering'):
-                n_clusters = st.slider('Number of Clusters', 2, 10, 2)
-                model = run_clustering(cleaned_df, n_clusters)
-                score = silhouette_score(cleaned_df, model.labels_)
-                st.write(f'Silhouette Score: {score:.2f}')
+            if is_cleaned
+                if st.button('Run Clustering'):
+                    n_clusters = st.slider('Number of Clusters', 2, 10, 2)
+                    model = run_clustering(cleaned_df, n_clusters)
+                    score = silhouette_score(cleaned_df, model.labels_)
+                    st.write(f'Silhouette Score: {score:.2f}')
         else:
             st.warning("Please cleansing data first.")
-  
+            is_cleaned = False
 if __name__ == '__main__':
     main()
