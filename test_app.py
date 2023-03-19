6354f872cfc8343e9b0903a0538919522613315a
Gimmixboi <127659666+Gimmixboi@users.noreply.github.com>
2023-03-19 10:23:25 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -60,19 +60,19 @@ def main():
         st.write("Uploaded file:")
         st.write(df.head())
         st.write(f"Data have {df.shape[0]} rows")
-        is_cleaned = False
         # Clean data
         if st.button('Cleansing data'):
             cleaned_df = clean_data(df)
-            is_cleaned = True
-        # clustering
-        if st.button('Run Clustering', key="run_clustering_btn") and is_cleaned:
             n_clusters = st.slider('Number of Clusters', 2, 10, 2)
             model = run_clustering(cleaned_df, n_clusters)
             score = silhouette_score(cleaned_df, model.labels_)
             st.write(f'Silhouette Score: {score:.2f}')
-        elif st.button('Run Clustering', key="run_clustering_btn"):
+        else:    
             st.warning("Please cleansing data first.")
+#         # clustering
+#         if st.button('Run Clustering'):
+            
+
             
 if __name__ == '__main__':
     main()
