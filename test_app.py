8ea063711a486d3a5b07a58f30f01d31315b6f28
Gimmixboi <127659666+Gimmixboi@users.noreply.github.com>
2023-03-19 09:38:47 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -27,10 +27,10 @@ def clean_data(df):
         st.write("Cleaned Dataset:")
         st.write(df.head())
         st.write(f"Data have {df.shape[0]} rows")
-        return df
+        cleaned_df = df
+        return cleaned_df
 
 def run_clustering(cleaned_df, n_clusters):
-#     cleaned_df = clean_data(df)
     with st.spinner("Program is Calculating,  ⏰ Please wait..."):
         wcss = []
         for i in range(2, 10):
@@ -61,20 +61,20 @@ def main():
         st.write(df.head())
         st.write(f"Data have {df.shape[0]} rows")
         # Clean data
-        cleaned_df = None
+#         cleaned_df = None
         if st.button('Cleansing data'):
             cleaned_df = clean_data(df)
         else:
             st.warning("Please cleansing data.")
         # clustering
         if st.button('Run Clustering'):
-            if cleaned_df is None:
-                st.warning("Please cleansing data first.")
-            else:
-                n_clusters = st.slider('Number of Clusters', 2, 10, 2)
-                model = run_clustering(cleaned_df, n_clusters)
-                score = silhouette_score(cleaned_df, model.labels_)
-                st.write(f'Silhouette Score: {score:.2f}')
+#             if cleaned_df is None:
+#             st.warning("Please cleansing data first.")
+#             else:
+            n_clusters = st.slider('Number of Clusters', 2, 10, 2)
+            model = run_clustering(cleaned_df, n_clusters)
+#             score = silhouette_score(cleaned_df, model.labels_)
+#             st.write(f'Silhouette Score: {score:.2f}')
             
 if __name__ == '__main__':
     main()
