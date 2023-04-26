9328679c03114761e2f6c18b67b33b43d6f1bb4f
Gimmixboi <127659666+Gimmixboi@users.noreply.github.com>
2023-04-26 16:23:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -76,19 +76,20 @@ def main():
         if uploaded_file is None:
             st.write("Warning:")
             st.warning("Please upload a file.")
+            cleaned_df = None
         else: 
             df = pd.read_csv(uploaded_file, encoding='ISO-8859-1')
             st.write("Uploaded file:")
-            st.write(df.head())
+            st.write(df.head(10))
             st.write(f"Data have {df.shape[0]} rows")
+  
     with tab2:
         st.subheader("Cleansing data")
-        cleaned_df = None
-        # Clean data
-        if button('Clean it up!', key="button1"):
-            cleaned_df = clean_data(df)
-        else:   
+        if cleaned_df is None:
             st.warning("Please cleansing data first.")
+        else:   
+            # Clean data
+            cleaned_df = clean_data(df)
             
 
 if __name__ == '__main__':
